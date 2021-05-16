9155022a2456906f807dfbce56e63b179e1e07cd
sajankafle26 <64882150+sajankafle26@users.noreply.github.com>
2021-05-16 12:29:00 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$('.carousel').carousel({
+	interval: 2000
+})
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items:7,
+  	loop:true,
+  });
+});
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+
+});
